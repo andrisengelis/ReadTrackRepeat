9903e0a560a474e3f16aeba04d1811b2eb12dfda
Andris Engelis <andris.engelis@outlook.com>
2019-02-17 19:19:53 +0200

    add completedBooks property to reading challenge

--- rtr.console/Program.cs
+++ rtr.console/Program.cs
@@ -13,6 +13,7 @@ namespace rtr.console
     class ReadingChallenge
     {
         public List<Book> ReadingList { get; set; }
+        public List<Book> BooksCompleted { get; set; }
         public int Year { get; set; }
         public int Goal { get; set; }
     }
@@ -29,11 +30,18 @@ namespace rtr.console
             challenge.ReadingList = new List<Book>(){
                 new Book { Title = "Killing Commendatore", Author = "Haruki Murakami", Pages = 927 },
                 new Book { Title = "Fall, or Dodge in Hell", Author = "Neal Stephenson", Pages = 720 },
-                new Book { Title = "White Shadow", Author = "Roy Jacobsen", Pages = 272 }
-            };
+                new Book { Title = "White Shadow", Author = "Roy Jacobsen", Pages = 272 },
+                new Book { Title = "Red Moon", Author = "Kim Stanley Robinson", Pages = 464 },
+                new Book { Title = "Summerland", Author = "Hannu Rajaniemi", Pages = 336 },
+                new Book { Title = "Diaspora", Author = "Greg Egan", Pages = 352 }
 
-            Console.WriteLine($"You have commited to read {challenge.Goal} books in {challenge.Year}");
-            Console.WriteLine($"You have {challenge.ReadingList.Count} books in your reading list for year {challenge.Year}");
+            };
+            challenge.BooksCompleted = new List<Book>();
+
+            Console.WriteLine($"This reading challenge is for year {challenge.Year}");
+            Console.WriteLine($"You have commited to read {challenge.Goal} books.");
+            Console.WriteLine($"You have completed {challenge.BooksCompleted.Count} books.");
+            Console.WriteLine($"You have {challenge.ReadingList.Count} books in your reading list");
         }
     }
 }
