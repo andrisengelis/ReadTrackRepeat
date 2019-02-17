10fd5152f82636faa3f5c50a8b5a9733c0cbecec
Andris Engelis <andris.engelis@outlook.com>
2019-02-17 19:03:23 +0200

    add ReadingChallenge class

--- rtr.console/Program.cs
+++ rtr.console/Program.cs
@@ -9,14 +9,28 @@ namespace rtr.console
         public string Author { get; set; }
         public int Pages { get; set; }
     }
+
+    class ReadingChallenge
+    {
+        public List<Book> Books { get; set; }
+        public int Year { get; set; }
+    }
     
     class Program
     {
         static void Main(string[] args)
         {
-            List<Book> YearChallange = new List<Book>();
+            ReadingChallenge challenge2018 = new ReadingChallenge();
 
-            Console.WriteLine("Hello World!");
+            challenge2018.Year = 2019;
+
+            challenge2018.Books = new List<Book>(){
+                new Book { Title = "Killing Commendatore", Author="Haruki Murakami", Pages=927 },
+                new Book { Title = "", Author="", Pages = 0}
+            };
+
+
+            Console.WriteLine($"You have {challenge2018.Books.Count} books in your reading list for year {challenge2018.Year}");
         }
     }
 }
