3a349cddde63b3831365e409fbc72f395b77ecda
Andris Engelis <andris.engelis@outlook.com>
2019-02-17 19:15:26 +0200

    add ReadingList property to reading challenge class

--- rtr.console/Program.cs
+++ rtr.console/Program.cs
@@ -12,7 +12,7 @@ namespace rtr.console
 
     class ReadingChallenge
     {
-        public List<Book> Books { get; set; }
+        public List<Book> ReadingList { get; set; }
         public int Year { get; set; }
         public int Goal { get; set; }
     }
@@ -26,14 +26,14 @@ namespace rtr.console
             challenge.Year = 2019;
             challenge.Goal = 24;
 
-            challenge.Books = new List<Book>(){
+            challenge.ReadingList = new List<Book>(){
                 new Book { Title = "Killing Commendatore", Author = "Haruki Murakami", Pages = 927 },
                 new Book { Title = "Fall, or Dodge in Hell", Author = "Neal Stephenson", Pages = 720 },
                 new Book { Title = "White Shadow", Author = "Roy Jacobsen", Pages = 272 }
             };
 
             Console.WriteLine($"You have commited to read {challenge.Goal} books in {challenge.Year}");
-            Console.WriteLine($"You have {challenge.Books.Count} books in your reading list for year {challenge.Year}");
+            Console.WriteLine($"You have {challenge.ReadingList.Count} books in your reading list for year {challenge.Year}");
         }
     }
 }
