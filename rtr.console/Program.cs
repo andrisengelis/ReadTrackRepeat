ecbbb14ed451992144ff92cf6e793ff4b1ca7ff1
Andris Engelis <andris.engelis@outlook.com>
2019-02-17 18:44:46 +0200

    add Book class, add List of Books for challange

--- rtr.console/Program.cs
+++ rtr.console/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace rtr.console
 {
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            List<Book> YearChallange = new List<Book>();
+
             Console.WriteLine("Hello World!");
         }
     }
