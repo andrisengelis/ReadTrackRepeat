a539fcb9dcc9364332b09eb3288d6f11a9833889
Andris Engelis <andris.engelis@outlook.com>
2019-11-08 23:54:29 +0200

    Add RTR Tests project

---
+++ ReadTrackRepeat.Tests/RtrApiTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReadTrackRepeat.Tests
+{
+  [TestClass]
+  public class RtrApiTests
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
