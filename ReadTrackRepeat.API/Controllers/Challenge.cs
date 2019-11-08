989af9219a3833ce50a9e7499f4682bf16260dd7
Andris Engelis <andris.engelis@outlook.com>
2019-11-08 23:55:02 +0200

    Add Challenge controler

---
+++ ReadTrackRepeat.API/Controllers/Challenge.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReadTrackRepeat.API.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ChallengeController : ControllerBase
+  {
+    [HttpGet]
+    public Challenge Get()
+    {
+      return new Challenge(2019);
+    }
+  }
+}
